f74fe86412580ac38bf18e7159b01abc8c50d9c5
elysiarose607-droid <elysiarose607@gmail.com>
2025-08-30 06:25:12 -0700

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -48,7 +48,7 @@ export default function Footer({ onNavigate }: FooterProps) {
                     WebkitMaskPosition: "center",
                     maskPosition: "center",
                   }} />
-                <img src="/dffdf.png" alt="Inflow Logo" className="h-full w-auto opacity-0 pl-4" />
+                <img src="/dffdf.png" alt="Inflow Logo" className="h-full w-auto opacity-0 pl-14" />
               </div>
             </div>
             <div className="space-y-2">
